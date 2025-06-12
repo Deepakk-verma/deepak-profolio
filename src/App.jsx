ebdf74e5f5cf060bfb474f2b45699a43863c9e30
Deepak Verma <mr.deepakverma7526@gmail.com>
2025-06-12 05:45:47 +0530

    🚀 Fix SignUp import and deploy-ready setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { NavLink, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Contact from "./pages/Contact";
-import Projects from "./pages/Projects"; // ✅ only this one should stay
-import SignUp from "./pages/Signup"; // ✅ Correct
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
 import Education from "./pages/Education";
- // ✅ NEW
 import "./App.css";
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
         <nav className="navbar">
           <NavLink to="/" end>Home</NavLink>
           <NavLink to="/projects">Projects</NavLink>
-          <NavLink to="/signUp">Sign Up</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink> {/* ✅ Fixed route casing */}
           <NavLink to="/login">Login</NavLink>
           <NavLink to="/education">Education</NavLink>
           <NavLink to="/contact">Contact</NavLink>
@@ -27,9 +26,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} /> {/* ✅ Matching route */}
           <Route path="/education" element={<Education />} />
           <Route path="/contact" element={<Contact />} />
-           <Route path="/signup" element={<SignUp />} /> {/* ✅ NEW */}
         </Routes>
       </main>
 
